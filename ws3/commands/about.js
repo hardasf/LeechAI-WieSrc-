7c1efa99cd2bb94ca986556a9f90939fa671bba2
Hard ASF <167669047+hardasf@users.noreply.github.com>
2024-10-19 12:54:49 +0800

    Update about.js

--- ws3/commands/about.js
+++ ws3/commands/about.js
@@ -1,5 +1,5 @@
 module.exports = {
-  description: "What is WieAI?",
+  description: "What is LeechAI?",
   async run({ api, send, admin }){
     await send({
       attachment: {
@@ -15,28 +15,24 @@ module.exports = {
         type: "template",
         payload: {
           template_type: "button",
-          text: `🤖 About WieAI:
-WieAI is your friendly, helpful personal assistant.
+          text: `🤖 About LeechAI:
+LeechAI is your friendly, helpful personal assistant.
 
-💭 Why I named WieAI as a name of the page bot because this is dedicated to my girlfriend itself 👸
+💭 This project is forked from WieAI 
 
 ❓ Contact us admins if you experienced/encountered any issue regarding to the bot and I will try to fix it. Thankyou for using me as a personal assistant!`,
           buttons: [
             {
               type: "web_url",
-              url: "https://www.facebook.com/profile.php?id=61566907376981",
+              url: "https://www.facebook.com/leechshares",
               title: "Like/Follow our Page"
                 },
             {
               type: "web_url",
-              url: "https://www.facebook.com/kennethfranciscoaceberos",
+              url: "https://www.facebook.com/imyourbaby.zxc000hhh",
               title: "Contact Admin 1"
-                },
-            {
-              type: "web_url",
-              url: "https://www.facebook.com/wieginesalpocialechavez",
-              title: "Contact Admin 2"
                 }
+
              ]
         }
       }
